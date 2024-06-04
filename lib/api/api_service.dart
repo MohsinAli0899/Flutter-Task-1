5494a75f68f07034862fe6387ce97def9aa27c21
itstabrez <tabrezamrath@gmail.com>
2024-06-05 00:50:03 +0530

    Updating url

--- lib/api/api_service.dart
+++ lib/api/api_service.dart
@@ -6,10 +6,10 @@ Future<Map<String, dynamic>> login(String username, String password) async {
   String getBaseUrl() {
     if (kIsWeb) {
       // URL for web
-      return "http://127.0.0.1:5000/proxy/login/$username,$password";
+      return "http://127.0.0.1:5001/proxy/login/$username,$password";
     } else {
       // URL for Android emulator
-      return "http://10.0.2.2:5000/proxy/login/$username,$password";
+      return "http://10.0.2.2:5001/proxy/login/$username,$password";
     }
   }
 
