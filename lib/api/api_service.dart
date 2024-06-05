12f8f3a6077066113bf6209787a2db7618537c93
MohsinAli0899 <mohsin.saifi98@gmail.com>
2024-06-05 12:09:45 +0530

    changes in API

--- lib/api/api_service.dart
+++ lib/api/api_service.dart
@@ -1,9 +1,8 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:flutter/foundation.dart';
 
 Future<Map<String, dynamic>> login(String username, String password) async {
-  String getBaseUrl() {
+ /* String getBaseUrl() {
     if (kIsWeb) {
       // URL for web
       return "http://127.0.0.1:5001/proxy/login/$username,$password";
@@ -13,21 +12,21 @@ Future<Map<String, dynamic>> login(String username, String password) async {
     }
   }
 
-  final baseUrl = Uri.parse(getBaseUrl());
-  // final url =
-  //     Uri.parse('http://127.0.0.1:5000/proxy/login/$username,$password');
+  final baseUrl = Uri.parse(getBaseUrl());  */
+  final baseUrl =
+  Uri.parse('https://development-gold.vercel.app/login/$username,$password');
 
   final response = await http.get(baseUrl);
 
   if (response.statusCode == 200) {
     return {
       "success": true,
-      "prompt": jsonDecode(response.body)['prompt'],
+      "Prompt": jsonDecode(response.body)['Prompt'],
     };
   } else {
     return {
       "success": false,
-      "prompt": jsonDecode(response.body)['Prompt'],
+      "Prompt": jsonDecode(response.body)['Prompt'],
     };
   }
 }
